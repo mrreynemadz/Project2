fa71713e5b2480d3a1da4bef24564185876b01db
mrreynemadz <161555351+mrreynemadz@users.noreply.github.com>
2024-06-16 20:11:27 +0800

    change

--- MODULE/Image_Classification/img_classification_app.py
+++ MODULE/Image_Classification/img_classification_app.py
@@ -1,28 +1,28 @@
 import pickle
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+import streamlit as st
+# from rembg import remove
 from PIL import Image
 from io import BytesIO
-from img2vec_pytorch import Img2Vec
-import streamlit as st
-import os
+import base64
 
 #NOTE don't forget to upload the picke (model) file to your Google Colab First
 #to run this code
 #you can use any model that is capable of classifiying images that uses img2vec_pytorch
-current_dir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(current_dir, 'model_needs_npk.p')
-with open(filename, 'rb') as f:
+with open('/content/drive/MyDrive/MADRONA-ITEQMT-FINAL-PROJECT/Dataset/ImageClassification/model_needs_npk.p', 'rb') as f:
     model = pickle.load(f)
+
 
 img2vec = Img2Vec()
 
-## Streamlit Web App Interface
-st.set_page_config(layout="wide", page_title="Cat Breed's Image Classification")
+# Streamlit Web App Interface
+st.set_page_config(layout="wide", page_title="Cat Breeds Classifier")
 
 st.write("## This is a demo of an Image Classification Model in Python!")
 st.write(
     ":grin: We'll try to predict the image on what features it was trained via the uploaded image :grin:"
 )
-st.sidebar.write("## Upload and download :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
@@ -40,18 +40,14 @@ def fix_image(upload):
     col1.image(image)
 
     col2.write("Category :wrench:")
-    img = Image.open(my_upload)
+    img = Image.open(upload)
     features = img2vec.get_vec(img)
     pred = model.predict([features])
 
-    # print(pred)
     col2.header(pred)
-    # st.sidebar.markdown("\n")
-    # st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
-
 
 col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg", "jfif"])
+my_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg", "jfif"])
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
@@ -59,4 +55,4 @@ if my_upload is not None:
     else:
         fix_image(upload=my_upload)
 else:
-    st.write("by koalatech...")
+    st.write("by reyne...")

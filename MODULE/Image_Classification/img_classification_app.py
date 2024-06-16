e4cd7b8d95009f4d19f7e144e4bad0060a62a1c4
mrreynemadz <161555351+mrreynemadz@users.noreply.github.com>
2024-06-16 20:23:46 +0800

    Update img_classification_app.py

--- MODULE/Image_Classification/img_classification_app.py
+++ MODULE/Image_Classification/img_classification_app.py
@@ -2,18 +2,21 @@ import pickle
 from img2vec_pytorch import Img2Vec
 from PIL import Image
 import streamlit as st
-# from rembg import remove
-from PIL import Image
 from io import BytesIO
-import base64
 import os
 
-#NOTE don't forget to upload the picke (model) file to your Google Colab First
-#to run this code
-#you can use any model that is capable of classifiying images that uses img2vec_pytorch
-with open('./model_needs_npk.p', 'rb') as f:
-    model = pickle.load(f)
+# File path for the pickled model
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, 'model_needs_npk.p')
 
+# Load the model
+try:
+    with open(file_path, 'rb') as f:
+        model = pickle.load(f)
+except FileNotFoundError:
+    st.error(f"Error: File '{file_path}' not found.")
+except Exception as e:
+    st.error(f"Error opening file '{file_path}': {str(e)}")
 
 img2vec = Img2Vec()
 
@@ -21,20 +24,19 @@ img2vec = Img2Vec()
 st.set_page_config(layout="wide", page_title="Cat Breeds Classifier")
 
 st.write("## This is a demo of an Image Classification Model in Python!")
-st.write(
-    ":grin: We'll try to predict the image on what features it was trained via the uploaded image :grin:"
-)
+st.write(":grin: We'll try to predict the image on what features it was trained via the uploaded image :grin:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-# Download the fixed image
-@st.cache_data
+# Function to convert image to bytes
+@st.cache(allow_output_mutation=True)
 def convert_image(img):
     buf = BytesIO()
-    img.save(buf, format="jpg")
+    img.save(buf, format="JPEG")
     byte_im = buf.getvalue()
     return byte_im
 
+# Function to process uploaded image
 def fix_image(upload):
     image = Image.open(upload)
     col1.write("Image to be predicted :camera:")
